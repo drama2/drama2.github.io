662b36764cd9b7b0cef96d6a1ee022ecf673f56e
Aevann1 <59999695+Aevann1@users.noreply.github.com>
2021-08-29 06:17:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ if(p !== null) {
     window.location.href='https://rdrama.net/post/'+p;
 }
 else if(c !== null) {
-    window.location.href='https://rdrama.net/comment/'+c+'/?context=5#context';
+    window.location.href='test';
+    //window.location.href='https://rdrama.net/comment/'+c+'/?context=5#context';
 }
 else {
     //probably a better way to do this
